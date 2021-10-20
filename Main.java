b56ab27a3ca21daeb4fd000d3d8f4b151b49b95c
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2021-10-20 11:08:42 -0700

    Q1

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Random rand = new Random(); // instance of random class
+    int upperbound = 50;
+    // generate random values from 0-50
+
+    ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+    myNumbers.add(rand.nextInt(upperbound));
+
+    System.out.println(myNumbers);
+
+    Scanner sc = new Scanner(System.in); // System.in is a standard input stream
+    System.out.print("What number are you looking for?");
+    int userNumber = sc.nextInt();
+
+    System.out.println("Is your Number in the list? " + myNumbers.contains(userNumber));
+
+    sc.close();
+  }
+}
